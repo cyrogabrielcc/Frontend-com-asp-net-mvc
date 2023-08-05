02fdc8efa730a516f551d90ecfb5ee8045f41527
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-04 21:37:25 -0300

    ModelsContext

--- Models/Contato.cs
+++ Models/Contato.cs
@@ -7,6 +7,10 @@ namespace MVC.NET.Models
 {
     public class Contato
     {
-        
-    }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public bool Ativo { get; set; }
+
+    } 
 }
