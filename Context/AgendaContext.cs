8ad84d52cef71aa0227eab4632e946ec736112c5
cyrogabrielcc <cyrocunhacg@gmail.com>
2023-08-05 08:43:23 -0300

    first commit

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DotNetRazorMVC.Models;
+
+namespace DotNetRazorMVC.Context
+{
+    public class AgendaContext : DbContext {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base (options) 
+        { 
+
+        }
+            public DbSet<Contato> Contatos { get; set; }
+        }
+}
+    
